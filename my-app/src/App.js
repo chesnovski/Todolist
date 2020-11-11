537b76bb4a9c8fbc00ddd59b13c5224736516349
Fedor <chesnovski99@gmail.com>
2020-11-11 13:17:55 +0100

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+import TODOList from './components/TODOList';
+
+const App = () => {
+
+  return (
+    <div >
+      <TODOList/>
+    </div>
+  )
+  }
+  export default App;
+ 
